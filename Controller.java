473a91dfc6ca1ca0aed1a44d4d5101dd542532eb
Baigalii <43229943+Baigalii@users.noreply.github.com>
2018-11-23 00:11:08 +0800

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,18 +1,25 @@
-package fastfood.food;
-
 import java.util.Scanner;
 
 public class Controller {
 	Food food = new Food("Khuushuur", 800.0, "Naadmiin khuushuur");
+	Food food1 = new Food("Buuz", 600.0, "Khachir-tai");
+	Food food2 = new Food("Hotdog", 3500.0, "Kola-tai");
 	Menu menu = new Menu();
+	Customer cust1 = new Customer("Baigal", 99210933, "BZD-6 khoroo");
 	Order order = new Order();
 	Order order2 = new Order();
 	
 	
 	public Controller() {
+
 		menu.addFood(food);
+		menu.addFood(food1);
+		menu.addFood(food2);
 		order.addMenu(menu);
+		order.addCustomer(cust1);
 		menu.showMenu();
+		
+		
 		System.out.print("Too hemjeeg oruulna uu: ");
 		Scanner sc = new Scanner(System.in);
 		order.setQuantity(sc.nextInt());
@@ -26,5 +33,5 @@ public class Controller {
 	public static void main(String[] args) {
 		new Controller();
 	}
-
+	
 }
