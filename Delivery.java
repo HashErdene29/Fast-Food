87d59b40f1ae6491fb61b3df997e967e131ba384
HashErdene29 <33370807+HashErdene29@users.noreply.github.com>
2018-11-23 09:01:02 +0800

    Add files via upload

---
+++ Delivery.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Delivery {
+	private String ReadyForDelivery;
+	private String PreparingForDelivery;
+	private String OnTheWayDelivery;
+	private String Delivered;
+	
+	
+	public String getReadyForDelivery() {
+		return ReadyForDelivery;
+	}
+	public void setReadyForDelivery(String readyForDelivery) {
+		ReadyForDelivery = readyForDelivery;
+	}
+	public String getPreparingForDelivery() {
+		return PreparingForDelivery;
+	}
+	public void setPreparingForDelivery(String preparingForDelivery) {
+		PreparingForDelivery = preparingForDelivery;
+	}
+	public String getOnTheWayDelivery() {
+		return OnTheWayDelivery;
+	}
+	public void setOnTheWayDelivery(String onTheWayDelivery) {
+		OnTheWayDelivery = onTheWayDelivery;
+	}
+	public String getDelivered() {
+		return Delivered;
+	}
+	public void setDelivered(String delivered) {
+		Delivered = delivered;
+	}	
+}
